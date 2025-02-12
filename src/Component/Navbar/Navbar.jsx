7f9a80a6066253bdd1330829fd001ff9e3daa221
Ofentse Sithole <otsithole07@gmail.com>
2025-02-12 12:05:07 +0200

    changed from href to link

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,37 +1,31 @@
-import {useRef} from 'react';
-import {FaBars, FaTimes} from "react-icons/fa";
+// Navbar.jsx
+import { useRef } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
-	const navRef = useRef();
+    const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-	};
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    };
 
-	return (
-		<header>
-			<h3>LOGO</h3>
-			<nav ref={navRef}>
-				<a href="/#">Home</a>
-				<a href="/#">My work</a>
-				<a href="/#">Blog</a>
-				<a href="/#">About me</a>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button
-				className="nav-btn"
-				onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
-	);
+    return (
+        <header className="header">
+            <Link to="/" className="logo">Logo</Link>
+            <nav ref={navRef}>
+                <Link to="/home">Home</Link>
+                <Link to="/contact">Contact</Link>
+
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <X />
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                <Menu />
+            </button>
+        </header>
+    );
 }
 
 export default Navbar;
