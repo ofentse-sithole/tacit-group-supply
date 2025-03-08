ea7de3679017a98f82407fb99570a1c4868d1255
Ofentse Sithole <124260201+ofentse-sithole@users.noreply.github.com>
2025-03-08 12:46:54 +0000

    added timer mobile responsive and website

--- src/Component/pages/Home.jsx
+++ src/Component/pages/Home.jsx
@@ -1,10 +1,12 @@
 import '../../App.css'
 import HeroSection from '../HeroSection/HeroSection'
 import Services from '../MiddleSection/ServiceMiddle'
+import Timer from '../HeroSection/TimerHeoSection'
 
 function Home() {
   return (
     <div>
+      <Timer/>
         <HeroSection />
         <Services/>
     </div>
